ac93dc2ba93b401bc5a52a8ebb76d2e020710bc2
林政浩 <1392872316@qq.com>
2022-10-23 22:19:05 +0800

    添加项目文件。

---
+++ stringf.c
@@ -0,0 +1,17 @@
+/* stringf.c -- 字符串格式 */
+#include <stdio.h>
+#define BLURB "Authentic imitation!"
+int main(void) {
+	printf("[%2s]\n", BLURB);
+	printf("[%24s]\n", BLURB);
+	printf("[%24.5s]\n", BLURB);
+	printf("[%-24.5s]\n", BLURB);
+
+	return 0;
+}
+
+//输出结果：
+//[Authentic imitation!]
+//[    Authentic imitation!]
+//[                   Authe]
+//[Authe                   ]
